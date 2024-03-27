f83139a32cfe1ed47fc8cc3766f6163d8b1d9044
BilRos <billyrossowuor@gmail.com>
2024-03-27 16:20:22 +0300

    I have used functions within functions and used const to asign to functions

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(){
+return scuberDrivers.slice(0,2);
+}
+
+const scuberDrivers=['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnLastTwoDrivers = function(){
+return scuberDrivers.splice(2);
+}
+
+const selectingDrivers=[returnFirstTwoDrivers , returnLastTwoDrivers];
+
+
+
+const createFareMultiplier=function (multiplier){
+       return function(integer){
+           return multiplier * integer;
+       } ;
+    }
+    
+
+
+const fareDoubler = function createFareMultiplier(fare){
+     return fare*2
+}
+
+const fareTripler = function createFareMultiplier(fare){
+    return fare*3
+}
+
+const selectDifferentDrivers = function(scuberDrivers , returnLastTwoDrivers){
+if(returnLastTwoDrivers === true) {
+    return returnLastTwoDrivers;
+}
+else {
+return scuberDrivers.slice(0,2);
+}
+}
